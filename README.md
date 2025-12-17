e56c9f86a160723215995c4e7bb7bab333417525
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-12-17 19:15:32 +0530

    add the async in function

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+1. Step one 
+cargo new project_name
+cd project_name
+
+2. Step two
+cargo add reqwest --features json
+cargo add tokio --features full
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
+#[tokio::main]
+async fn main() {
+    let response = reqwest::get("").await.unwrap().text().await.unwrap();
+    println!("{}" , response);
     println!("Hello, world!");
 }
