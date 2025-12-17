08a1ab5619b2894c763215a5a491dd7ae1016384
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-12-17 19:31:23 +0530

    search engine index text add

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use scraper::{Html , Selector};
 async fn main() {
     let html = reqwest::get("https://example.com").await.unwrap().text().await.unwrap();
     let document = Html::parse_document(&html);
-    let selector = Selector::parse("title").unwrap();
-    for element in document.select(&selector){
-        let title = element.inner_html();
-         println!("Title : {}" , title);
+    let body_selector = Selector::parse("body").unwrap();
+    for body in document.select(&body_selector){
+        let text = body.text().collect::<Vec<_>>().join(" ");
+        println!("Page Text :\n{}" , text);
     }
    
     println!("Hello, world!");
