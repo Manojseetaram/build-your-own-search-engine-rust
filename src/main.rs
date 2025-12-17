40afc2b90be83fb74c3c844a7cbeeab1260ec150
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-12-17 21:37:28 +0530

    Extract the link

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ use scraper::{Html , Selector};
 async fn main() {
     let html = reqwest::get("https://example.com").await.unwrap().text().await.unwrap();
     let document = Html::parse_document(&html);
-    let body_selector = Selector::parse("body").unwrap();
-    for body in document.select(&body_selector){
-        let text = body.text().collect::<Vec<_>>().join(" ");
-        println!("Page Text :\n{}" , text);
+    let link_selector = Selector::parse("a").unwrap();
+    println!("Links found : ");
+    for element in document.select(&link_selector){
+    //    if let text = body.text().collect::<Vec<_>>().join(" ");
+     if let Some(link) = element.value().attr("href"){
+    println!("{}" , link);
+     }
+        // println!("Page Text :\n{}" , text);
     }
    
     println!("Hello, world!");
